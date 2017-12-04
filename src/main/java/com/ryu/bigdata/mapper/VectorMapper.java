7cde5be0bfeaa70f2bd45b437b54757cb10741b0
kimyongyeon <toyongyeon@gmail.com>
2017-12-04 06:57:45 +0000

    수정.

--- src/main/java/com/ryu/bigdata/mapper/VectorMapper.java
+++ src/main/java/com/ryu/bigdata/mapper/VectorMapper.java
@@ -1,9 +1,6 @@
 package com.ryu.bigdata.mapper;
 
-import com.ryu.bigdata.vo.Product;
 import com.ryu.bigdata.vo.SkuImgVo;
-import com.ryu.bigdata.vo.WeeklySales;
-import com.ryu.bigdata.vo.WeeklySalesSearch;
 import org.mapstruct.Mapper;
 
 import java.util.List;
